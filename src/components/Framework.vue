a7fde77cdca757a3ca8a2b0692cef9a4f6cc8913
赵劲焰 <949474952@qq.com>
2019-01-27 18:59:03 +0800

    创建framework组件

---
+++ src/components/Framework.vue
@@ -0,0 +1,34 @@
+<template>
+  <Layout>
+    <Header>Header</Header>
+    <Layout :style="{padding: '0 50px'}">
+      <Breadcrumb :style="{margin: '16px 0'}">
+        <BreadcrumbItem>Home</BreadcrumbItem>
+        <BreadcrumbItem>Components</BreadcrumbItem>
+        <BreadcrumbItem>Layout</BreadcrumbItem>
+      </Breadcrumb>
+      <Content :style="{padding: '24px 0', minHeight: '280px', background: '#fff'}">
+        <Layout>
+          <Sider hide-trigger :style="{background: '#fff'}">
+            <AsyncMenu></AsyncMenu>
+          </Sider>
+          <Content :style="{padding: '24px', minHeight: '280px', background: '#fff'}">Content</Content>
+        </Layout>
+      </Content>
+    </Layout>
+    <Footer>2011-2016 &copy; TalkingData</Footer>
+  </Layout>
+</template>
+
+<script>
+import AsyncMenu from "@/components/AsyncMenu.vue";
+/*
+ 框架控件，负责总体布局
+*/
+export default {
+  name: "Framework",
+  components: {
+    AsyncMenu
+  }
+};
+</script>
