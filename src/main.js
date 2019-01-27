c2a4b59a4c438e292c4751f77d3fded351d7d083
赵劲焰 <949474952@qq.com>
2019-01-27 18:58:42 +0800

    引用iview库

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import iview from "iview";
+import 'iview/dist/styles/iview.css';
+
+Vue.use(iview);
 
 Vue.config.productionTip = false;
 
