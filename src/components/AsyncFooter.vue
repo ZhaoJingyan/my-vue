f69d4d5b34acf03beb8061f328c271560044b031
赵劲焰 <949474952@qq.com>
2019-01-28 18:10:56 +0800

    构建AsyncFooter

---
+++ src/components/AsyncFooter.vue
@@ -0,0 +1,17 @@
+<template>
+    <Footer class="layout-footer-center">2011-2016 &copy; TalkingData</Footer>
+</template>
+
+<script>
+export default {
+    name: "AsyncFooter"
+};
+</script>
+
+<style scoped>
+.layout-footer-center {
+    text-align: center;
+}
+</style>
+
+
