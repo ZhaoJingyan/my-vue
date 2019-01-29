f9ad9a93a4931087fc57cfe9d1f59842c5ba6d94
赵劲焰 <949474952@qq.com>
2019-01-29 15:43:19 +0800

    格式化代码

--- src/views/AccountFilesUploadView.vue
+++ src/views/AccountFilesUploadView.vue
@@ -1,28 +1,24 @@
 <template>
     <div>
         <Title title="Upload Account Files"></Title>
-        <Upload
-        multiple
-        type="drag"
-        action="//jsonplaceholder.typicode.com/posts/">
-        <div style="padding: 20px 0">
-            <Icon type="ios-cloud-upload" size="52" style="color: #3399ff"></Icon>
-            <p>Click or drag files here to upload</p>
-        </div>
-    </Upload>
-
+        <Upload multiple type="drag" action="//jsonplaceholder.typicode.com/posts/">
+            <div style="padding: 20px 0">
+                <Icon type="ios-cloud-upload" size="52" style="color: #3399ff"></Icon>
+                <p>Click or drag files here to upload</p>
+            </div>
+        </Upload>
     </div>
 </template>
 
 <script>
-import Title from '@/components/Title.vue'
+import Title from "@/components/Title.vue";
 
 export default {
-    name: 'AccountFilesUploadView',
-    components:{
+    name: "AccountFilesUploadView",
+    components: {
         Title
     }
-}
+};
 </script>
 
 
