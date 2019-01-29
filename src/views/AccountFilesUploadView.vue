296fcc2c947239566b88e930587bda4292e0e2e1
赵劲焰 <949474952@qq.com>
2019-01-29 15:38:46 +0800

    添加上传控件

--- src/views/AccountFilesUploadView.vue
+++ src/views/AccountFilesUploadView.vue
@@ -1,12 +1,27 @@
 <template>
     <div>
-        <h1>Upload Account Files</h1>
+        <Title title="Upload Account Files"></Title>
+        <Upload
+        multiple
+        type="drag"
+        action="//jsonplaceholder.typicode.com/posts/">
+        <div style="padding: 20px 0">
+            <Icon type="ios-cloud-upload" size="52" style="color: #3399ff"></Icon>
+            <p>Click or drag files here to upload</p>
+        </div>
+    </Upload>
+
     </div>
 </template>
 
 <script>
+import Title from '@/components/Title.vue'
+
 export default {
-    name: 'AccountFilesUploadView'
+    name: 'AccountFilesUploadView',
+    components:{
+        Title
+    }
 }
 </script>
 
