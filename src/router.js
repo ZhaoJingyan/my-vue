72b0a94d73c9259ed8b3d36bf1149b4315e04330
赵劲焰 <949474952@qq.com>
2019-02-11 16:28:57 +0800

    删除默认router

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
 
 Vue.use(Router);
 
@@ -11,24 +10,15 @@ export default new Router({
     {
       path: "/",
       name: "home",
-      component: Home
+      component: () => import("./views/Home")
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
-    },
-    {
-      path: "/accountBalance/filesUpload",
+      path: "/my/accountBalance/filesUpload",
       name: "accountBalance-filesUpload",
       component: () => import("./views/AccountFilesUploadView.vue")
     },
     {
-      path: "/accountBalance/",
+      path: "/my/accountBalance/",
       name: "accountBalance",
       component: () => import("./views/AccountBalanceView.vue")
     }
